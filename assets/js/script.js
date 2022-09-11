c78dd8e1ae625d07e192eef8df8e17f9e351ed03
Zack <zack@Zacks-MBP.lan>
2022-09-11 14:48:13 -0400

    added form submit handler to js and added needed id's to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,29 @@
 var foodRecipe = document.querySelector('#food-recipe-list'); // unordered list for food recipes (parent of li child)
 var foodButton = document.querySelector('#food-button'); // food recipe submit button
-var foodInput = document.querySelector('#food-input'); // food input value
+var foodForm = document.querySelector('#food-form'); // food input value
+var foodInput = document.querySelector('#food-input')
 var drinkRecipe = document.querySelector('#drink-recipe-list'); // unordered list for drink recipes (parent of li child)
 var drinkButton = document.querySelector('#drink-button'); // drink recipe submit button
 var drinkInput = document.querySelector('#drink-input'); // drink input value
 
-// inside fetch API function
-var FoodRequestUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast'; // =chicken will be replaced with '= + meal' from search
+// form submission for recipes based on input ingredient
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+  
+    var ingredient = foodInput.value.trim(); // id of input element for submit form
+  
+    if (ingredient) {
+      getUserRepos(ingredient);
+  
+      foodRecipe.textContent = ''; // where the ingredients list goes
+      foodInput.value = '';
+    } else {
+      alert('Please enter an ingredient');
+    }
+  };
 
+  
+  
 // create a new list item for the unordered list
 // content of the new list item (comes from input value)
 // appending the part of the html that will be updated (include classes to match html - establish separately in css)
